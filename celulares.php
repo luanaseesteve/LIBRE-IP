82d4723005a413514e57a9a3603f86e0a0188fa9
luanaesteve <luanaesteve@hotmail.com>
2024-04-14 17:35:44 -0300

    inicio de trabajo final

---
+++ celulares.php
@@ -0,0 +1,38 @@
+<?php
+
+function baseDatosEmpresa(){
+    BDProductos
+}
+function comprador(){
+    CarritoCompras
+}
+function datosVentas(){
+    ArregloVentas
+}
+function codigo(){
+
+}
+function marca(){
+
+}
+function modelo(){
+
+}
+function cantidadEjemplares(){
+
+}
+function precioUnitario(){
+    
+}
+
+
+/**El programa deberá inicialmente solicitar un nombre de usuario y contraseña al usuario: Nombres de usuario
+“invitado” con contraseña “invitado” son clientes de la empresa. Otros nombres de usuarios son
+administradores: “admin” con contraseña “admin”
+Los administradores podrán realizar las siguientes tareas de acuerdo al siguiente menú (En cada opción
+invocará a distintas funciones para cumplir la funcionalidad, teniendo en cuenta los requerimientos de
+implementación). Con subrayado se resalta la opción del menú y se realiza una descripción de lo que se
+pretende implementar (En algunos casos se indican explícitamente las funciones, en otras queda a cargo del
+alumno determinar qué funciones utilizar). */
+
+?>
